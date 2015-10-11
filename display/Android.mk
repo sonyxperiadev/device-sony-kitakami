480bcc6fbabb4023cd2fc94d2d950a8a08b2efc2
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-10-11 18:04:25 +0200

    remove local display HAL
    we can use hardqare/qcom/display HAL
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- display/Android.mk
+++
@@ -1,5 +0,0 @@
-
-display-hals := libgralloc libcopybit libmemtrack libqservice libqdutils
-display-hals += libgenlock libhwcomposer liboverlay libhdmi
-
-include $(call all-named-subdir-makefiles,$(display-hals))
