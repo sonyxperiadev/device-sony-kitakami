35f8774d55905f09887c89f8919e18a2169d7511
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-01-24 09:32:39 +0100

    update PERMGR
    use qcom default TARGET_PER_MGR_ENABLED
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,6 +104,8 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUEDROID_VENDOR_CONF := device/sony/kitakami/bluetooth/vnd_generic.txt
 
+# RIL
+TARGET_PER_MGR_ENABLED := true
 
 # GPS definitions for Qualcomm solution
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
