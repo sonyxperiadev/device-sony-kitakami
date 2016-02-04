19cc7b7fcef2f835608d75f1cabf4b837f4f3ae8
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-04 23:35:48 +0100

    move common bootimg to common
    move common KERNEL_CMDLINE and init.*.rc to common git
    for easy maintenance
    
    move device HALs to device
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

---
+++ rootdir/Android.mk
@@ -0,0 +1,53 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.kitakami
+LOCAL_SRC_FILES := fstab.kitakami
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := fstab.kitakami
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.$(TARGET_DEVICE)
+LOCAL_SRC_FILES := init.kitakami.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := init.$(TARGET_DEVICE)
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.kitakami.pwr
+LOCAL_SRC_FILES := init.kitakami.pwr.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := init.kitakami.pwr
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.recovery.$(TARGET_DEVICE)
+LOCAL_SRC_FILES := init.recovery.kitakami.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := init.recovery.$(TARGET_DEVICE)
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ueventd.$(TARGET_DEVICE)
+LOCAL_SRC_FILES := ueventd.kitakami.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := ueventd.$(TARGET_DEVICE)
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+$(shell mkdir -p $(PRODUCT_OUT)/root && pushd $(PRODUCT_OUT)/root > /dev/null && ln -s fstab.kitakami fstab.$(TARGET_DEVICE) && popd > /dev/null)
+
