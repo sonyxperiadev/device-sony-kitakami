c3466160bdb85dfddaa32d57356acbf14f5ec682
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-04-19 15:53:37 +0200

    remove unused keylayout configs
    configs were added during bringup and can be removed
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- platform.mk
+++ platform.mk
@@ -43,7 +43,6 @@ PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/system/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(SONY_ROOT)/system/usr/keylayout/mhl-rcp.kl:system/usr/keylayout/mhl-rcp.kl \
     $(SONY_ROOT)/system/usr/keylayout/msm8994-tomtom-snd-card_Button_Jack.kl:system/usr/keylayout/msm8994-tomtom-snd-card_Button_Jack.kl \
-    $(SONY_ROOT)/system/usr/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
     $(SONY_ROOT)/system/usr/keylayout/touch_fusion.kl:system/usr/keylayout/touch_fusion.kl
 
 # QMI
