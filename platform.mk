35683b27b8c1d3d4f1ee47202b931d213a70251e
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-10-02 16:23:03 +0200

    Revert "move rild.libpath to common"
    we want to override the common path with the legacy path
    
    This reverts commit 99b97842ecfc6d19e61a01b7d663d47e88fb0031.

--- platform.mk
+++ platform.mk
@@ -96,6 +96,11 @@ PRODUCT_PACKAGES += \
     brcm-uim-sysfs \
     libfmjni
 
+# RILD
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ril.subscription.types=NV,RUIM
+
 # system prop for opengles version
 # 196609 is decimal for 0x30001 to
 # report major/minor versions as 3/1
