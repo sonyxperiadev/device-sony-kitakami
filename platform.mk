b9f9e81fb6ad7c8d21f361ddf028690121b41149
Richard Folke Tullberg <richardfolke.tullberg@sony.com>
2018-01-18 09:22:19 +0100

    Enable fluence for enhanced in-call audio quality

--- platform.mk
+++ platform.mk
@@ -112,6 +112,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196609
 
+# Fluence
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.sdk.audio.fluencetype=fluence
+
 # aDSP sensors
 ## max rate
 PRODUCT_PROPERTY_OVERRIDES += \
